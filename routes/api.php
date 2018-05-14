0907ee7b7654769ec169c8c33fb00989002e3606
RyannnnnnR <ryan.reichenberg@gmail.com>
2018-05-14 18:50:37 +1000

    UPDATE: Fix routing

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('restaurants', 'RestaurantsController');
-Route::resource('countries', 'CountriesController');
-Route::resource('roles', 'RolesController');
-Route::resource('categories', 'CategoriesController');
-Route::resource('posts', 'PostsController');
-Route::resource('comments', 'CommentsController');
-Route::resource('users', 'UsersController');
+Route::get('restaurants/countryandcategory', 'RestaurantsController@findRestaurantWithCountryAndCategory');
+Route::get('restaurants/postsandcomments', 'RestaurantsController@restaurantWithPostsAndComments');
+Route::post('restaurants/store', 'RestaurantsController@store');
+Route::put('restaurants/update', 'RestaurantsController@update');
+Route::delete('restaurants/destroy','RestaurantsController@destroy');
+
+Route::post('roles/store', 'RolesController@store');
+Route::put('roles/update', 'RolesController@update');
+Route::delete('roles/destroy','RolesController@destroy');
+
+Route::post('categories/store', 'CategoriesController@store');
+Route::put('categories/update', 'CategoriesController@update');
+Route::delete('categories/destroy','CategoriesController@destroy');
+
+
+Route::get('comments', 'CommentsController@index');
+Route::post('comments/store', 'CommentsController@store');
+Route::put('comments/update', 'CommentsController@update');
+Route::delete('comments/destroy','CommentsController@destroy');
+
+Route::get('posts', 'PostsController@index');
+Route::post('posts/store', 'PostsController@store');
+Route::put('posts/update', 'PostsController@update');
+Route::delete('posts/destroy','PostsController@destroy');
+
+Route::post('users/store', 'UsersController@store');
+Route::put('users/update', 'UsersController@update');
+Route::delete('users/destroy','UsersController@destroy');
